5b472d79a4a5ebbd59eba4435bdbf1a8cae16c20
meryem boubghy <meryemboubghy02@gmail.com>
2025-06-19 23:57:56 +0100

    Exercice6

---
+++ Week1/Day2/Exercices_XP/Exercice6.py
@@ -0,0 +1,7 @@
+def make_shirt(size="large",text="I love Python"):
+    print(f"The size of the shirt is {size}  and the text is  {text}")
+
+make_shirt("L","test")
+make_shirt("S")
+make_shirt("medium ")
+make_shirt(size="small",text="Custom message")
