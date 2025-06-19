2e5222388d6b329acf13fcc153feb5851b6adca3
meryem boubghy <meryemboubghy02@gmail.com>
2025-06-19 22:08:34 +0100

    Exercice 4

---
+++ Week1/Day2/Exercices_XP/Ex4.py
@@ -0,0 +1,7 @@
+def describe_city(city , country="Unknown"):
+  print(f" {city} is in  {country}")
+  
+describe_city("casablanca","Maroc")
+describe_city("Hanova","Allemand")
+describe_city("Sale")
+  
