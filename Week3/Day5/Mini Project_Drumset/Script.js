da085da5a989c68cbdfa6455684a2ffad664827f
meryem boubghy <meryemboubghy02@gmail.com>
2025-07-08 22:24:48 +0100

    Challenge 2

---
+++ Week3/Day5/Mini Project_Drumset/Script.js
@@ -0,0 +1,25 @@
+function playSound(key) {
+  const audio = document.querySelector(
+    `audio[data-key="${key.toUpperCase()}"]`
+  );
+  const keyDiv = document.querySelector(
+    `.container[data-key="${key.toUpperCase()}"]`
+  );
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+
+  keyDiv.classList.add("playing");
+  setTimeout(() => keyDiv.classList.remove("playing"), 150);
+}
+
+document.addEventListener("keydown", (e) => {
+  playSound(e.key);
+});
+
+document.querySelectorAll(".container").forEach((key) => {
+  key.addEventListener("click", () => {
+    playSound(key.getAttribute("data-key"));
+  });
+});
