0592b0dfa014033bcc724d730ca18a09a7393dbc
meryem boubghy <meryemboubghy02@gmail.com>
2025-07-10 15:22:01 +0100

    Ninja

---
+++ Week4/Day1/Exercices XP Ninja/Exercice1.js
@@ -0,0 +1,17 @@
+// function mergeWords(mot) {
+//   return function () {
+//     return "Hello";
+//   };
+// }
+// console.log(mergeWords("hello")());
+// 2 :
+function mergeWords(string) {
+  return function (nextString) {
+    if (nextString === undefined) {
+      return string;
+    } else {
+      return mergeWords(string + " " + nextString);
+    }
+  };
+}
+console.log(mergeWords("There")("is")("no")("spoon.")());
